d9d2d5cdea4325068e56ea8f99273fc7a9b83e28
Allegra Latimer <alatimer@stanford.edu>
2018-05-17 13:34:00 -0700

    created script to regenerate traj files from atoms objects; traj files stored in traj-files folder

--- atomic_coordinates.py
+++ atomic_coordinates.py
@@ -1,4 +1,15 @@
 import pickle
+from ase import io
 
 catlistobj = pickle.load(open('dftobj.pkl','rb'))
-catlistobj = catlistobj.filter(lambda x: x.ets_ch4!=None)
+catlistobj = catlistobj.filter(lambda x: x.atoms_ch4!=None)
+
+
+for cat in catlistobj.data:
+    print cat.cattype,cat.cat
+    for surf,atoms in zip(['slab','CH4-TS','CH3OH-TS'],[cat.atoms_slab,cat.atoms_ch4,cat.atoms_ch3oh]):
+        print surf
+        io.write('traj-files/%s-%s-%s.traj'%(cat.cat,cat.cattype,surf),atoms)
+        for atom in atoms:
+            print atom.symbol,round(atom.position[0],4),round(atom.position[1],4),round(atom.position[2],4)
+
