19dd009d4972c6cb63ac68fd6d639c32a3c2bb2b
Allegra Latimer <alatimer@stanford.edu>
2018-05-18 09:54:22 -0700

    added cif files for metals, oxides

--- atomic_coordinates.py
+++ atomic_coordinates.py
@@ -9,7 +9,7 @@ for cat in catlistobj.data:
     print cat.cattype,cat.cat
     for surf,atoms in zip(['slab','CH4-TS','CH3OH-TS'],[cat.atoms_slab,cat.atoms_ch4,cat.atoms_ch3oh]):
         print surf
-        io.write('traj-files/%s-%s-%s.traj'%(cat.cat,cat.cattype,surf),atoms)
+        io.write('traj-files/%s-%s-%s.cif'%(cat.cat,cat.cattype,surf),atoms)
         for atom in atoms:
             print atom.symbol,round(atom.position[0],4),round(atom.position[1],4),round(atom.position[2],4)
 
